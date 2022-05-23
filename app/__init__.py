f8dfae1ab22dd6ea1e82c83707d612c864e89a9a
shannan-liu1 <81000747+shannan-liu1@users.noreply.github.com>
2022-05-23 13:08:50 -0700

    delete extra stuff

--- app/__init__.py
+++ app/__init__.py
@@ -17,20 +17,3 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     return render_template('main.html')
-
-@app.route('/about/', methods=['POST', 'GET'])
-def about():
-    return render_template('about.html')
-
-# Request object: https://flask.palletsprojects.com/en/2.1.x/api/#flask.Request
-@app.route('/submit-basic/', methods=['POST', 'GET'])
-def submit_basic():
-    if request.method == 'GET':
-        return render_template('submit.html')
-    else:
-        try:
-            # this is how to access the upladed file
-            # img = request.files['image']
-            return render_template('submit.html',thanks = True)
-        except:
-            return render_template('submit.html', error = True)
