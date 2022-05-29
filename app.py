b58c46049b0690e56b9800219a3a891d93f50ffe
shannan-liu1 <81000747+shannan-liu1@users.noreply.github.com>
2022-05-28 19:22:55 -0700

    finishing hw

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, g, render_template, request
-from flask.cli import with_appcontext
-from werkzeug.security import check_password_hash, generate_password_hash
+import sqlite3 # for interfacing with our db
 
 import click
 
@@ -18,32 +17,25 @@ from matplotlib.figure import Figure
 import io
 import base64
 
-import sqlite3 # interface with the db
-
 app = Flask(__name__)
 @app.route('/')
 def main():
     return render_template('main.html')
 
-@app.route('/submit', methods=['POST', 'GET'])
-def submit():
-    if request.method == 'GET':
-        return render_template('submit.html')
-    else:
-
-        return render_template('submit.html')
-
 def get_message_db():
     """
     Handles creating database messages
     """
+    # check if message_db exists in the g attribute
+    # of our application
+    # if not, create it
     try:
-        return g.message_db
+        return g.message_db # return db if it already exists
     except:
-        # check if message_db exists in the g attribute
-        # of our application
-        # if not, create it
+        # create message db
         g.message_db = sqlite3.connect("message_db.sqlite")
+
+        # sql command to create table called messages in message db
         cmd = """
         CREATE TABLE IF NOT EXISTS messages(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -51,31 +43,79 @@ def get_message_db():
         message TEXT NOT NULL
         );
         """
+        
+        # execute the db command 
+        # with cursor object
         cursor = g.message_db.cursor()
         cursor.execute(cmd)
         return g.message_db
 
-
 def insert_message():
+    """
+    Called when the user submits their handle and message on the
+    submit page of our website
+    """
+
+    # use request object to get user's handle & message input
     handle = request.form['handle']
     message = request.form['message']
     db = get_message_db()
 
-    cmd = f"""\
-    INSERT INTO message_db (handle, message)
-    VALUES ({handle},{message})
+    cmd = f"""
+    INSERT INTO messages (handle, message) 
+    VALUES ('{handle}', '{message}')
     """
-    <input type="text" name="message" id="message">
 
+    # execute the db command 
+    # with cursor object
+    cursor = db.cursor()
+    cursor.execute(cmd)
 
-def close_message_db(e = None):
-    db = g.pop('message_db',None)
-    if db is not None:
-        db.close()
+    # save changes to db and close connection
+    db.commit()
+    db.close()
+    return handle, message
 
-def init_db():
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+
+    # show this page for when user doesn't make a submission
+    if request.method == 'GET':
+        return render_template('submit.html')
+    # otherwise, if the user has made a submission,
+    # show this page
+    else:
+        # call insert_message() 
+        # render template w/ new variables
+        # retrieved from intert_message() function
+        try:
+            handle, message = insert_message()
+            return render_template('submit.html', submitted=True, message=message, handle=handle)
+        except: # if an error occurred, render the webpage while specifying the error
+            return render_template('submit.html', error=True)
+
+def random_messages(n):
+    # open connection to db
     db = get_message_db()
 
+    # randomly select n handles and messages to display
+    cmd = f"""
+    SELECT * FROM table ORDER BY RANDOM() LIMIT {n}
+    """
 
+    # execute the db command 
+    # with cursor object
+    cursor = db.cursor()
+    cursor.execute(cmd)
 
-    #Check whether there is a database called message_db in the g attribute of the app. If not, then connect to that database, ensuring that the connection is an attribute of g. To do this last step, write a line like do g.message_db = sqlite3.connect("messages_db.sqlite")
+    # use fetchall to get the n randomly selected 
+    # handles and messages in our database
+    message_info = cursor.fetchall()
+    db.close() #  close connection
+    return message_info
+
+@app.route('/view/')
+def view():
+    # view messages
+    return render_template('view.html', messages=random_messages(10))
+    
